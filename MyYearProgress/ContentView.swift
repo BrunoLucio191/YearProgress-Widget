92264a58e0cb12876a4afdff32abd82c07b168ff
Ciolu <brunoestudos6@gmail.com>
2026-01-25 15:57:23 -0300

    Initial Commit

---
+++ MyYearProgress/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyYearProgress
+//
+//  Created by Bruno Lucio Silva Prado on 25/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyYearProgress/MyYearProgressApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyYearProgressApp.swift
+//  MyYearProgress
+//
+//  Created by Bruno Lucio Silva Prado on 25/01/26.
+//
+
+import SwiftUI
+
+@main
+struct MyYearProgressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
